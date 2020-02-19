dc74182dde2c6cf70027df5df5d6f352c00e9451
gim34 <gim34@naver.com>
2020-02-19 15:14:20 +0900

    Add  Asynchronous chat server

--- webSocket/chat/urls.py
+++ webSocket/chat/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    # url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
