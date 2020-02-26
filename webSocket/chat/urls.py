9b32fbc211da9d23d0b78a929dd0e68e8765510b
gim34 <gim34@naver.com>
2020-02-26 14:41:01 +0900

    Update a form

--- webSocket/chat/urls.py
+++ webSocket/chat/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('<str:menu>/', views.menu, name='menu'),
 ]
